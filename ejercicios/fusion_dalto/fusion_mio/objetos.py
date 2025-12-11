202e7f8c4324414b28aa8f556ac987f4407d7047
CarmenTM100 <carmentrejomancilla03548@gmail.com>
2025-12-10 19:03:42 -0600

    Unas modificaciones extras a objetos y juego

--- ejercicios/fusion_dalto/fusion_mio/objetos.py
+++ ejercicios/fusion_dalto/fusion_mio/objetos.py
@@ -4,3 +4,6 @@ class Objetos:
         self.cantidad = cantidad
     def __repr__(self):
         return f"{self.precio} - {self.cantidad}"
+    
+    def __str__(self):
+        return f"Representacion bonita Precio: {self.precio} y {self.cantidad}"
